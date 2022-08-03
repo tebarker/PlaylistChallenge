0deff6811b476c4a54a53f234053930a8bfba4bd
Tenae <tenae@Zipcoders-MacBook-Pro-107.local>
2022-08-03 14:20:49 -0400

    no test yet

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,42 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        int next = goingForward(startIndex, selection);
+        int back = goingBackwards(startIndex, selection);
+
+        if (back > next) {
+            return next;
+        } else {
+            return back;
+        }
     }
+
+    public Integer goingForward(Integer startIndex, String selection) {
+        int forwardCount = 0;
+        for (int i = startIndex; !playList[i].contains(selection); i++) {
+            if (i == playList.length - 1) {
+                i = -1;
+            }
+            forwardCount++;
+
+        }
+        return forwardCount;
+    }
+
+
+    public Integer goingBackwards(Integer startIndex, String selection){
+        int backCount = 0;
+        for(int i = startIndex; !playList[i].contains(selection); i--){
+            if(i == 0){
+                i = playList.length;
+            } backCount++;
+
+        } return backCount;
+    }
+
+
+
 }
+
+
